ef329a88f5e0924f178460577034830660b223df
Chance Smith <chancesmith97@hotmail.com>
2020-03-11 23:02:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
       
     elsif req.path.match(/cart/)
-      if @@cart.nil!
+        @@cart.nil!
         @@cart.each{|item| resp.write "#{item}\n"
       
         resp.write "Empty Cart"
